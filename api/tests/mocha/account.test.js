036322a2e6adf383c041b5159ee806296fab235a
new MH extractions <dave.ashehurst@evrythng.com>
2017-06-06 15:28:31 +0100

    mocha added

---
+++ api/tests/mocha/account.test.js
@@ -0,0 +1,25 @@
+var assert = require("assert");
+require("app-module-path").addPath("/users/dave/github/wotfing/api/modules");
+const utils = require("utils.js");
+const db = require("db.js");
+before(function() {
+	return db.Empty();
+});
+describe("Database Tests", function() {
+	describe("Account", function() {
+		it("should Create a New Account", function() {
+			return db
+				.CreateAccount({ email: "test@test.com", pwd: "qwepoi" })
+				.then(res => {
+					assert.equal(64, res.newUser.key.length);
+				});
+		});
+		it("should Login", function() {
+			return db
+				.Login({ email: "test@test.com", password: "qwepoi" })
+				.then(res => {
+					assert.equal(64, res.key.length);
+				});
+		});
+	});
+});
