8366a060372379ff562ac0c456982217a18414db
new MH extractions <dave.ashehurst@evrythng.com>
2017-09-09 10:41:40 +0100

    auto

--- api/tests/connectToPosgres.js
+++
@@ -1,8 +0,0 @@
-const pool = require("../lib/db");
-
-//to run a query we just pass it to the pool
-//after we're done nothing has to be taken care of
-//we don't have to return any client to the pool or close a connection
-pool.query("SELECT * FROM users", function(err, result) {
-	console.log("number of rows : ", result.rows.length);
-});
