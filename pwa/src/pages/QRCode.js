1cd536d325ed5610653da564bed4142a34315d4a
Dave Ashenhurst <dave.ashenhurst@evrythng.com>
2018-09-05 15:45:58 +0100

    Router Kind of Working with no style

--- pwa/src/pages/QRCode.js
+++ pwa/src/pages/QRCode.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class QrCode extends React.Component {
+  qrcode() {
+    return "QWE";
+  }
+
   render() {
     return (
       <div>
-        <h1>QR Code Scanned, {this.props.name}</h1>
+        <h1>QR Code Scanned is : {this.qrcode()}</h1>
         <div>
           <ul>
             <li>
